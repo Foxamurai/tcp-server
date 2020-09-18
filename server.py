16dbe982b1a440af0af037462ac98565bfecdfd5
Foxamurai <Foxamurai@gmail.com>
2020-09-19 01:21:19 +0400

    asyncio плавит мозг

--- server.py
+++ server.py
@@ -1,12 +1,107 @@
 import asyncio
-import time
+from collections import defaultdict
+from copy import deepcopy
+
+
+class StorageDriverError(ValueError):
+    pass
+
+
+class Storage:
+    """Класс для хранения метрик в памяти процесса"""
+
+    def __init__(self):
+        self._data = defaultdict(dict)
+
+    def put(self, key, value, timestamp):
+        self._data[key][timestamp] = value
+
+    def get(self, key):
+
+        if key == '*':
+            return deepcopy(self._data)
+
+        if key in self._data:
+            return {key: deepcopy(self._data.get(key))}
+
+        return {}
+
+
+class StorageDriver:
+    """Класс, предосталяющий интерфейс для работы с хранилищем данных"""
+
+    def __init__(self, storage):
+        self.storage = storage
+
+    def __call__(self, data):
+
+        method, *params = data.split()
+
+        if method == "put":
+            key, value, timestamp = params
+            value, timestamp = float(value), int(timestamp)
+            self.storage.put(key, value, timestamp)
+            return {}
+        elif method == "get":
+            key = params.pop()
+            if params:
+                raise StorageDriverError
+            return self.storage.get(key)
+        else:
+            raise StorageDriverError
+
+
+class MetricsStorageServerProtocol(asyncio.Protocol):
+    """Класс для реализации сервера при помощи asyncio"""
+
+    storage = Storage()
+    # настройки сообщений сервера
+    sep = '\n'
+    error_message = "wrong command"
+    code_err = 'error'
+    code_ok = 'ok'
+
+    def __init__(self):
+        super().__init__()
+        self.driver = StorageDriver(self.storage)
+        self._buffer = b''
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        """Метод data_received вызывается при получении данных в сокете"""
+
+        self._buffer += data
+
+        try:
+            request = self._buffer.decode()
+            # ждем данных, если команда не завершена символом \n
+            if not request.endswith(self.sep):
+                return
+
+            self._buffer, message = b'', ''
+            raw_data = self.driver(request.rstrip(self.sep))
+
+            for key, values in raw_data.items():
+                message += self.sep.join(f'{key} {value} {timestamp}' \
+                                         for timestamp, value in sorted(values.items()))
+                message += self.sep
+
+            code = self.code_ok
+        except (ValueError, UnicodeDecodeError, IndexError):
+            message = self.error_message + self.sep
+            code = self.code_err
+
+        response = f'{code}{self.sep}{message}{self.sep}'
+        # отправляем ответ
+        self.transport.write(response.encode())
 
 
 def run_server(host, port):
     loop = asyncio.get_event_loop()
-    coroutine = loop.create_server(Server, host, port)
-
-    server = loop.run_until_complete(coroutine)
+    coro = loop.create_server(MetricsStorageServerProtocol, host, port)
+    server = loop.run_until_complete(coro)
 
     try:
         loop.run_forever()
@@ -18,58 +113,5 @@ def run_server(host, port):
     loop.close()
 
 
-class Storage:
-    """Класс, выполняющий роль хранилища метрик"""
-
-    def __init__(self):
-        self._data = {}
-
-    def put(self, key, value, timestamp=None):
-        timestamp = timestamp or int(time.time())
-        if key not in self._data:
-            self._data[key] = {}
-        self._data[key][timestamp] = value
-
-    def get(self, key):
-        data = self._data
-
-        result = "ok\n"
-
-        if key == "*":
-            for key in data.keys():
-                for timestamp, value in sorted(data[key].items()):
-                    result += f"{key} {timestamp} {value}\n"
-            result += "\n"
-
-        return result
-
-
-class Server(asyncio.Protocol):
-    def __init__(self):
-        self.storage = Storage()
-
-    def connection_made(self, transport):
-        self.transport = transport
-
-    def process_data(self, data):
-        request = data[:-1].split()
-        command = request[0]
-        query = request[1:]
-
-        if command == "put":
-            key, value, timestamp = query
-            self.storage.put(key, value, timestamp)
-            return "ok\n\n"
-        if command == "get":
-            key = query[0]
-            raw_data = self.storage.get(key)
-            response = ""
-
-    def data_received(self, data):
-        request = self.process_data(data.decode())
-        self.transport.write(request.encode())
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_server("127.0.0.1", 8888)
