e4c97a5a1aecb60836e26f694c904ac6c3cfc04e
Foxamurai <foxamura@gmail.com>
2020-09-18 11:31:41 +0400

    пфффффффф

--- server.py
+++ server.py
@@ -2,6 +2,22 @@ import asyncio
 import time
 
 
+def run_server(host, port):
+    loop = asyncio.get_event_loop()
+    coroutine = loop.create_server(Server, host, port)
+
+    server = loop.run_until_complete(coroutine)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+
 class Storage:
     """Класс, выполняющий роль хранилища метрик"""
 
@@ -12,18 +28,49 @@ class Storage:
         timestamp = timestamp or int(time.time())
         if key not in self._data:
             self._data[key] = {}
-
         self._data[key][timestamp] = value
 
     def get(self, key):
         data = self._data
 
-        if key != "*":
-            data = {key: data.get(key, {})}
+        result = "ok\n"
 
-        result = {}
-
-        for key, timestamp_data in data.items():
-            result[key] = sorted(timestamp_data.items())
+        if key == "*":
+            for key in data.keys():
+                for timestamp, value in sorted(data[key].items()):
+                    result += f"{key} {timestamp} {value}\n"
+            result += "\n"
 
         return result
+
+
+
+class Server(asyncio.Protocol):
+    def __init__(self):
+        self.storage = Storage()
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def process_data(self, data):
+        request = data[:-1].split()
+        command = request[0]
+        query = request[1:]
+
+        if command == "put":
+            key, value, timestamp = query
+            self.storage.put(key, value, timestamp)
+            return "ok\n\n"
+        if command == "get":
+            key = query[0]
+            raw_data = self.storage.get(key)
+            response = ""
+
+    def data_received(self, data):
+        request = self.process_data(data.decode())
+        self.transport.write(request.encode())
+
+
+
+if __name__ == '__main__':
+    run_server("127.0.0.1", 8888)
